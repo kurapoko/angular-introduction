2b36c125933b97b8d8474ca4cc98f689305a3480
m.kurahara <kuraharamasahiro@gmail.com>
2020-01-22 10:54:14 +0900

    FormBuilderについて

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-hello',
@@ -10,23 +10,18 @@ export class HelloComponent implements OnInit {
   title: string;
   message: string;
   myControl: FormGroup;
-
-  constructor() { 
+  constructor(private fb: FormBuilder) { 
 
   }
 
   ngOnInit() {
     this.title = "Hello-app";
     this.message = 'FormGroupを使う';
-    this.myControl = new FormGroup({
-      name: new FormControl(''),
-      mail: new FormControl(''),
-      age: new FormControl(0),
-      control: new FormControl(),
-      control2: new FormControl(),
-      control3: new FormControl(),
-      control4: new FormControl()
-    }); 
+    this.myControl = this.fb.group({
+      name: [''],
+      mail: [''],
+      age: [0]
+    });
   }
 
   onSubmit() {
